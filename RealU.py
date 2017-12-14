504fd5e5031cbc41445ae57512a6289622401b80
JackFram <erdos_zzh@163.com>
2017-12-14 17:01:30 +0800

    change the cfg file

--- RealU.py
+++ RealU.py
@@ -1,8 +1,8 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-app = Flask(__name__, instance_path="/Users/zhangzhihao/Documents/RealU/instance", instance_relative_config=True)
-app.config.from_pyfile('RealU_Config.py')
+app = Flask(__name__)
+app.config.from_pyfile('instance/RealU_Config.py')
 
 
 @app.route('/')
