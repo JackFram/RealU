488bd8d9c9356b216aa342c27080ee20fc5cc617
JackFram <erdos_zzh@163.com>
2017-12-15 22:17:34 +0800

    DATABASE_URL updated

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = '?\xbf,\xb4\x8d\xa3"<\x9c\xb0@\x0f5\xab,w\xee\x8d$0\x13\x8b83'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///posts.db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class DebugConfig(BaseConfig):
